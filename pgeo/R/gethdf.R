bb15cfd41678905f77f62f9b6870449aa2ccb177
pascal-niklaus <pascal@niklaus.org>
2022-12-06 07:59:58 +0100

    re-built for new R version

--- pgeo/R/gethdf.R
+++ pgeo/R/gethdf.R
@@ -6,8 +6,8 @@
 #' \code{get_hdf_list} retrieves the URL of all MODIS hdf files from
 #' Aqua, Terra, or both and returns them as a \code{data.frame}.  It
 #' is possibly to specify a MODIS file version (currently 6, the
-#' default) and to narrow down the list by specifying a date interval
-#' (argument \code{dates}).
+#' default) and to narrow down the list by specifying a year interval
+#' (argument \code{years}).
 #'
 #' \code{get_hdf_file} retrieves on or more HDF files.
 #' \code{username} and \code{password} to access the server need to be
@@ -24,19 +24,20 @@
 #' @param sat the MODIS instruments for which data is retrieved,
 #'     e.g. \code{aqua} or \code{terra}, or both (default).
 #'
-#' @param dates a date interval to which hdf file retrieval should be
-#'     restricted. Start and end dates are provided as
-#'     \code{yyyy.mm.dd}.  To retrieve the entire data of year 2010,
-#'     one could use \code{dates=c('2010.00.00','2010.99.99')}.
+#' @param years an interval to which hdf file retrieval should be
+#'     restricted. Start and end years are provided as
+#'     \code{yyyy}.  To retrieve the entire data of year 2010,
+#'     one could use \code{years='2010'}.
 #'
 #' @param version MODIS file version, currently 6 (default).
 #'
-#' @param server server to use, defaults to \code{http://e4ftl01.cr.usgs.gov}.
+#' @param server server to use, including base path. defaults to
+#'     \code{https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/}.
 #'
 #' @param product product to retrieve, defaults to \code{13Q1}.
 #'
-#' @param verbose logical indication whether progress should be logged
-#'     to the console (defaults to \code{TRUE}).
+#' @param verbose integer indicating verbosity. Values supported are
+#'     0, 1 and 2
 #'
 #' @param url full url of MODIS file to be downloaded.
 #'
@@ -46,11 +47,15 @@
 #' @param file if specifies, \code{file} allows to change the file
 #'     name under which the MODIS hdf file is saved.
 #'
-#' @param username,password username and password for hdf file
-#'     retrieval.
+#' @param appkey App Key for data access, exported rom the LAADS DAAC
+#'     site (https://ladsweb.modaps.eosdis.nasa.gov/profile/#app-keys)
 #'
 #' @param overwrite \code{get_hdf_file} skips the download of files
 #'     that already exist on disk, unless \code{overwrite=TRUE}.
+#'
+#' @param cacheage indicates whether a cache for the URL directories
+#'     should be used. Positive values indicate the maximum age in
+#'     hours.
 #'
 #' @return A data frame with columns \code{url}, \code{dir}, and
 #'     \code{file}.
@@ -58,79 +63,168 @@
 #' @examples
 #' \dontrun{
 #' require(pgeo);
-#' h <- get_hdf_list(dates=c("2010.00.00","2010.01.14"))
-#' get_hdf_files(h$url, username='user', password='secret')
+#' h <- get_hdf_list(yearss=c("2010","2015"))
+#' get_hdf_files(h$url, addkey='xxxx')
 #' }
 #'
 #' @rdname gethdf
 #' @importFrom utils head
 #' @importFrom utils tail
+#' @importFrom pascal safen
+#'
 #' @export
-get_hdf_list <- function(tiles = c("h18v04"),
-                       sat=c("aqua","terra"),
-                       dates=c("0000.00.00","9999.99.99"),
-                       version=6,
-                       product="13Q1",
-                       server="http://e4ftl01.cr.usgs.gov",
-                       verbose=TRUE)
+get_hdf_dirs <-
+    function(
+             sat=c("aqua","terra"),
+             years=c("0000","9999"),
+             version=6,
+             product="13Q1",
+             server="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/",
+             verbose=1,
+             cacheage=24)
 {
-    if(verbose)
+    ## helper with negative indiced for substring 'backwards'
+    myss <- function(x, start, stop) {
+        x <- as.character(x)
+        if (start > 0)
+            substr(x, start, stop)
+        else {
+            n <- nchar(x)
+            substr(x,n+start, n+stop)
+        }
+    }
+
+    srvr <- gsub("(?<!/)/[^/].+","",server,perl=TRUE)
+    years <- c(if(is.na(head(years, 1))) "0000" else head(years,1),
+               if(is.na(tail(years, 1))) "9999" else tail(years,1))
+    years <- sprintf("%04d", safen(years))
+
+    ## get top directories for each year
+    topdirs <-
+        unlist(
+            lapply(
+                sat,
+                function(sat) {
+                    isAqua <-  grepl("^[Aa]", sat, perl=TRUE)
+                    MODISurl <- sprintf("%s/%d/%s%s",
+                                        server,
+                                        version,
+                                        if(isAqua) "MYD" else "MOD",
+                                        product)
+                    while (TRUE) {
+                        if(verbose > 0)
+                            cat("  Retrieving ",MODISurl,"...\n")
+                        r <- try(readLinesCached(MODISurl, maxage=cacheage))
+                        if (class(r) != "try-error")
+                            break
+                        warning("      *** Error retrieving",
+                                MODISurl,
+                                "... retrying after 10 minute...\n")
+                        Sys.sleep(600)
+                    }
+
+                    ## filter out lines with links to subdirectories
+                    r <- gsub("^.*href=\\\"([^\\\"]+)\\\".*$","\\1",r);
+                    idx <- grepl(sprintf("%s/[0-9]{4}/$",product), r, perl=TRUE);
+                    r <- r[idx]
+
+                    r <- Filter(
+                        function(i) {
+                            yr <- myss(i, -4, -1)
+                            yr >= myss(years[1],1,4) & yr <= myss(years[2],1,4)
+                        },
+                        r)
+                }))
+
+    ## scan all top-level directories and extract subdirectories
+    unlist(
+        lapply(topdirs,
+               function(d) {
+                   if(verbose > 0)
+                       cat("  Inspecting directory '",d,"'...\n",sep="")
+                   dirurl <- sprintf("%s%s", srvr, d)
+                   while (TRUE) {
+                       try(subdirs <- readLinesCached(dirurl, maxage=cacheage))
+                       if(class(subdirs) != "try-error")
+                           break
+                       warning("        *** Error retrieving",
+                               dirurl,
+                               "... retrying after 1 minute...\n")
+                       Sys.sleep(60)
+                   }
+                   subdirs <- gsub("^.*href=\\\"([^\\\"]+)\\\".*$","\\1", subdirs);
+                   idx <- grepl("/\\d{4}/\\d{3}/$", subdirs, perl=TRUE)
+                   subdirs[idx]
+               }))
+}
+
+#' @rdname gethdf
+#' @export
+get_hdf_list <-
+    function(tiles = c("h18v04"),
+             sat=c("aqua","terra"),
+             years=c("0000","9999"),
+             version=6,
+             product="13Q1",
+             server="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/",
+             verbose=1,
+             cacheage=24)
+{
+    years <- c(if(is.na(head(years, 1))) "0000" else head(years,1),
+               if(is.na(tail(years, 1))) "9999" else tail(years,1))
+    years <- sprintf("%04d", safen(years))
+
+    srvr <- gsub("(?<!/)/[^/].+","",server,perl=TRUE)
+
+    if(verbose > 0)
         cat("Acquiring list of HDF files for tile(s)",
             paste(tiles,collapse=", "),
             "...\n")
 
+    dirs <- get_hdf_dirs(
+        sat=sat,
+        years=years,
+        version=version,
+        product=product,
+        server=server,
+        verbose=verbose,
+        cacheage=cacheage)
+
     hdflist <- NULL
 
-    dates <- c(if(is.na(head(dates, 1))) "0000.00.00" else head(dates,1),
-               if(is.na(tail(dates, 1))) "9999.99.99" else tail(dates,1))
+    ## loop over top-level dirs and get hdf files
+    urls <-
+        unlist(
+            sapply(
+                dirs,
+                function(sd) {
+                    if(verbose > 0)
+                        cat("  Scanning",sd,"\r")
+                    ff <- readLinesCached(sprintf("%s%s", srvr, sd),
+                                          maxage = cacheage)
+                    fx <- gsub("^.*href=\\\"([^\\\"]+\\.hdf)\\\".*$","\\1", ff);
+                    idx <- grepl("/\\d{4}/\\d{3}/.+h\\d{2}v\\d{2}.+hdf",
+                                 fx,
+                                 perl=TRUE)
+                    fx[idx]
+                },
+                USE.NAMES = FALSE))
 
-    dates <- gsub("^(\\d{4}).*(\\d{2}).*(\\d{2})$",
-                  "\\1.\\2.\\3",
-                  dates,
-                  perl=TRUE)
 
-    for(sat_ in sat) {
-        isAqua <-  grepl("^[Aa]", sat_, perl=TRUE)
-        MODISurl <- sprintf("%s/%s/%s%s.%03d/",
-                            server,
-                            if(isAqua) "MOLA" else "MOLT",
-                            if(isAqua) "MYD" else "MOD",
-                            product,
-                            version)
-        if(verbose)
-            cat("  Base URL:",MODISurl,"\n")
-        r <- readLines(MODISurl);
+    ## extract file name and limit to selected tiles
+    files <- gsub("^.+/","", urls, perl=TRUE);
+    idx <- grepl(sprintf("^.+(%s)\\.%03d\\..+hdf$",
+                         paste(tiles,collapse="|"),
+                         version),
+                 files,
+                 perl=TRUE)
+    files <- files[idx]
+    urls <- urls[idx]
 
-        ## filter out lines with links to subdirectories
-        dirs <- gsub("^.*href=\\\"([^\\\"]+)\\\".*$","\\1",r);
-        idx <- grepl("^[0-9]{4}\\.[0-9]{2}\\.[0-9]{2}/?$",dirs,perl=TRUE);
-        dirs <- dirs[idx]
-
-        dirs <- Filter(function(i) i>=dates[1] && i<=dates[2], dirs)
-
-        for(d in dirs) {
-            if(verbose)
-                cat("    Inspecting directory '",d,"'...\n",sep="")
-            try(f <- readLines(dirurl<-sprintf("%s%s",MODISurl, d)))
-            while(class(f) == "try-error")
-                warning("      *** Error retrieving", dirurl, "... retrying...\n");
-            files <- gsub("^.*href=\\\"([^\\\"]+)\\\".*$","\\1", f);
-            idx <- grepl(sprintf("^.+(%s)\\.%03d\\..+hdf$",
-                                 paste(tiles,collapse="|"),
-                                 version),
-                         files,
-                         perl=TRUE)
-            files <- files[idx]
-            if(verbose)
-                for(j in seq_along(files))
-                    cat("      FOUND: ",files[j],"\n");
-            hdflist <- rbind(hdflist,
-                             data.frame(url=sprintf("%s%s",dirurl,files),
-                                        dir=d,
-                                        file=files))
-        }
-    }
-    hdflist
+    ## retrun as data frame
+    data.frame(url=sprintf("%s%s",srvr,urls),
+               file=files,
+               stringsAsFactors=FALSE)
 }
 
 #' @rdname gethdf
@@ -138,27 +232,28 @@ get_hdf_list <- function(tiles = c("h18v04"),
 get_hdf_file <- function(url=NULL,
                          file=NULL,
                          outdir=NULL,
-                         username="user",
-                         password="passw",
+                         appkey=NULL,
+                         verbose=0,
                          overwrite=FALSE)
 {
     if(is.null(file))
         file <- gsub("^.+/", "", url, perl=TRUE)
-    system("echo `pwd`")
-    system(sprintf("touch .netrc && chmod 0600 .netrc && echo machine urs.earthdata.nasa.gov login %s password %s >.netrc",
-                   username,
-                   password))
-    system("touch .urs_cookies && chmod 0600 .urs_cookies")
+
     for(i in seq_along(url)) {
-        fullname <- if(is.null(outdir)) file[i] else paste(outdir, file[i], sep="/")
+        fullname <- if(is.null(outdir))
+                        file[i]
+                    else
+                        paste(outdir, file[i], sep="/")
         if(overwrite || !file.exists(fullname)) {
-            cat("Retrieving", file[i], "...\n")
-            system(sprintf("curl -o '%s' -O -b .urs_cookies -c .urs_cookies --location --netrc-file .netrc '%s'",
+            cmd <- sprintf("curl %s -H 'Authorization: Bearer %s' -o '%s' -O '%s'",
+                           if(verbose > 1) "-v" else "",
+                           appkey,
                            fullname,
-                           url[i]))
+                           url[i])
+            if(verbose > 0)
+                cat("Command:",cmd,"\n")
+            system(cmd)
         } else
             cat("File",file,"already exists, skipping...\n")
     }
-    unlink(".urs_cookies")
-    unlink(".netrc")
 }
